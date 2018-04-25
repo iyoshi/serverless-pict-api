d49eeef3beeaed5c583ddef6379e000340150501
iyoshi <iwai0621@gmail.com>
2018-04-25 23:54:12 +0900

    Introduce delete-image API

---
+++ src/delete-image.py
@@ -0,0 +1,78 @@
+import json
+import logging
+import os
+
+import boto3
+from botocore.client import ClientError
+
+from src.decimalencoder import DecimalEncoder
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb', region_name=os.getenv('AWS_DEFAULT_REGION'))
+table = dynamodb.Table(os.getenv('PHOTOS_TABLE_NAME'))
+
+def handler(event, context):
+    """
+    TBD.
+
+    :param event: TBD
+    :param context: TBD
+    :return: TBD
+    """
+
+    path_params = event['pathParameters']
+
+    if 'image_id' not in path_params:
+
+        logger.error("Validation Failed")
+
+        return {
+            'statusCode': 400,
+            'body': 'Required params is invalid',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+
+    photo_id = path_params['image_id']
+
+    try:
+        result = table.get_item(Key={ 'photo_id': photo_id })
+
+        if 'Item' not in result:
+            return {
+                'statusCode': 404,
+                'body': 'Resource not found',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+
+        table.delete_item(Key={ 'photo_id': photo_id })
+
+    except ClientError as err:
+        logger.error(err.response['Error']['Code'])
+
+        return {
+            'statusCode': 500,
+            'body': err.response['Error']['Code'],
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+
+    logger.info('photo_id = {} is successfully deleted.'.format(photo_id))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({ 'photo_id': photo_id }),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
