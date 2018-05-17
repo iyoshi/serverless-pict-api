354dd3a3a597bb840a88fa68a91e1ec63e874077
eeeeyy <iwai0621@gmail.com>
2018-05-17 20:36:30 +0900

    Add a region to boto3 calls and fix unit test

--- tests/testing/dynamodb_testing_util.py
+++ tests/testing/dynamodb_testing_util.py
@@ -6,11 +6,11 @@ class DynamoDbTestingUtil:
     @staticmethod
     def create_dynamodb_local_resource():
 
-        return boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+        return boto3.resource('dynamodb', endpoint_url='http://localhost:8000', region_name='us-east-1')
 
     @staticmethod
     def delete_table(table_name):
 
-        boto3.client('dynamodb', endpoint_url='http://localhost:8000').delete_table(
+        boto3.client('dynamodb', endpoint_url='http://localhost:8000', region_name='us-east-1').delete_table(
             TableName=table_name
         )
